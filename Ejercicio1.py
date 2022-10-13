85ffb0eb48923099afc9280f306d87dbd29c64d5
dmunofer <diego.fmz@hotmail.com>
2022-10-13 22:26:25 +0200

    cambios

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -3,4 +3,11 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
 
-def ej1(y,t):
+def ej1(y,x):
+    dydt= (x**2*y-y)/(1+y)
+    return dydt
+
+y0=-1
+t = np.linspace(0,5)
+
+def resuelta():
