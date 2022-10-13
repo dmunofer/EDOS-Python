3173bc209db7ef6a7aa9c50ff2594acaa9adfb30
dmunofer <diego.fmz@hotmail.com>
2022-10-13 22:36:54 +0200

    cambios

---
+++ Ejercicio3.py
@@ -0,0 +1,13 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+from math import *
+import sympy
+
+def ej3(y,x):
+    dydt = sympy(2*(x-2)**2*(y/(x-2)))
+    return dydt
+
+t = np.linspace(0,5)
+def resuelta3():
+    resuelta= odeint(ej3,t)
